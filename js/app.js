5ff3b4406c1c0de1e5929231a2895c6df670a73f
Lu Juemin <jmlu0619@gmail.com>
2015-10-12 22:14:49 +0800

    top reduced

--- js/app.js
+++ js/app.js
@@ -11,16 +11,13 @@
 		var navs = $("#nav div");
 			var text = navs.eq(0).find("a").html();
 
-			console.log(text);
 			navs.eq(0).mouseenter(function(){
-				$(this).find("a").html("TOP");
 				navs.eq(1).css({bottom:'100px'});
 				navs.eq(2).css({bottom:'170px'});
 				navs.eq(3).css({bottom:'240px'});
 
 			});
 			$("#nav").mouseleave(function(){
-				navs.eq(0).find("a").html(text);
 				navs.eq(1).css({bottom:'30px'});
 				navs.eq(2).css({bottom:'30px', right:'30px'});
 				navs.eq(3).css({bottom:'30px'});
