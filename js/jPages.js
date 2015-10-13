3a8ff8b86443a5ae8fd9b80734a1e4b927824999
Lu Juemin <jmlu0619@gmail.com>
2015-10-13 14:56:10 +0800

    post added

--- js/jPages.js
+++ js/jPages.js
@@ -20,17 +20,19 @@
 		jPage_box_shadow = this.css('boxShadow');
 		jPage_color = this.css("backgroundColor");
 		jPage_position = this.css("position");
+		jPage_margin = this.css("margin");
 		jWrapper = $("<div></div>").addClass("jWrapper").css({
 			display:'inline-block', 
 			position:jPage_position,
-			paddingLeft:-jPage_left,
+			padding:-jPage_left,
 			paddingRight:-jPage_right,
 			paddingTop:-jPage_top,
 			paddingBottom:-jPage_bototm,
 			left:jPage_left,
 			right:jPage_right,
 			top:jPage_top,
-			bottom:jPage_bototm
+			bottom:jPage_bototm,
+			margin:jPage_margin
 		});
 		this.css({top:0, left:0, bottom:0, right:0, zIndex:2})
 
@@ -48,6 +50,11 @@
 				backgroundColor:jPage_color,
 				boxShadow:jPage_box_shadow
 			});
+			// if(jPage_position !== "absolute"){
+			// 	jPages[i].css({
+			// 		top:-jPage_height
+			// 	});
+			// }
 		}
 	}
 })(jQuery);
